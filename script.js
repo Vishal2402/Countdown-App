c5a8d10c158b6af6d7e0f614aced405131b94f01
Vishal2402 <70733643+Vishal2402@users.noreply.github.com>
2020-10-20 12:30:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const newYear = "1 Jan 2021";
+
+
+
+function countdown(){
+    const newYearsDate =  new Date(newYear);
+    const curDate =  new Date();
+    const seconds = (newYearsDate - curDate)/1000;
+    const Days = Math.floor(seconds/60/60/24);
+    const Hours = Math.floor((seconds-(Days*86400))/3600); 
+    const Minutes = Math.floor((seconds - ((Days*86400)+(Hours*3600))) / 60);
+    const Seconds = Math.floor(seconds - ((Days*86400)+(Hours*3600)+(Minutes*60)));
+    document.getElementById("Days").innerText = Days; 
+    document.getElementById("HOURS").innerText = Hours; 
+    document.getElementById("MINUTES").innerText = Minutes; 
+    document.getElementById("SECONDS").innerText = Seconds; 
+    console.log(Days,
+        Hours,
+        Minutes,
+        Seconds);
+}
+    countdown();
+    setInterval(countdown,1000);
